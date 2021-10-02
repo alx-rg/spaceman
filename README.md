94f94f0ada7a039121c48c3bde0e1afb9f05d86a
Alex Ross <alex.ross@shopify.com>
2021-10-02 16:02:13 -0500

    filled in Readme and started pseudo code on spaceman

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # spaceman
+
+Play the spaceman game:
+Follow the on screen prompts
+Guess correctly and win.
+Guess wrong enough time and lose.
+The end
---
+++ spaceman.py
@@ -0,0 +1,15 @@
+
+
+
+#Below are aspects of the program that will be required
+
+#The game needs to explain what's going to happen with the player.
+#How to play, how many guesses
+
+#Randomness that will require us to import the random module
+
+#Take in a list of words from a text document and pick one that will be guessed
+
+#Once the words is selected, function to hide the secret word and replace the length with underscores
+#Return the secret word
+
