5e2f41d36e8588d80811ecb69b958672429ab911
Alex Ross <alex.ross@shopify.com>
2021-10-03 13:19:25 -0500

    added word.txt file and import of random and random word definition

--- spaceman.py
+++ spaceman.py
@@ -8,6 +8,8 @@
 
 #Randomness that will require us to import the random module
 
+import random
+
 #Take in a list of words from a text document 
 #Function to return a word from the word list
 #Function to hide the secret word and replace the length with underscores
@@ -15,5 +17,16 @@
 #Function to print the underscores to the screen
 #Return the secret word
 
+def import_word():
+   #import a list of words from text file and READ list
+   #place the list of words into a variable
+   #close the list of words text file.
+   #in the variable, select at random a word
+   word_list = open('words.txt', 'r')
+   secret_word_list = word_list
+   secret_word = random.choice(secret_word_list)
+   #word_list.close('words.txt')
+   return secret_word
 
+print(import_word())
 
